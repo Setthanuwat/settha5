ad5b8410e81e9533fa2d52fce788b03a9e0aeec8
Setthanuwat <aftnick2@gmail.com>
2021-12-13 17:19:11 +0700

    "lap10_1"

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -1,15 +1,16 @@
 #include<iostream>
-#include<iomanip> //For using setw(), setprecision(), ...
+#include<iomanip> 
 using namespace std;
 
 int main(){	
+	double m, r, p, mf, mr, x, y, ml;
+	double i = 1 ;
 	cout << "Enter initial loan: ";
+	cin >> m ;
 	cout << "Enter interest rate per year (%): ";
+	cin >> r ;
 	cout << "Enter amount you can pay per year: ";
-
-	//use 'setw' to set width of table and 'left' to set left-alignment
-	//you can change input argument of 'setw()' to see the effect
-	//Try to change from 'left' to 'right' and see the effect
+	cin >> p ;
 	cout << setw(13) << left << "EndOfYear#"; 
 	cout << setw(13) << left << "PrevBalance"; 
 	cout << setw(13) << left << "Interest"; 
@@ -17,17 +18,45 @@ int main(){
 	cout << setw(13) << left << "Payment";
 	cout << setw(13) << left << "NewBalance";
 	cout << "\n";
-	
-	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
-	//you can change input argument of 'setprecision()' to see the effect
-	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
-	cout << "\n";	
+	ml = m; 
+	while(m > 0)
+	{
+		if(ml > p)
+		{
+			m = ml;
+			mr = m*r/100 ;
+			mf = m+mr;
+			ml = mf - p;
+			cout << fixed << setprecision(2); 
+			cout << setw(13) << left << i; 
+			cout << setw(13) << left << m;
+			cout << setw(13) << left << mr;
+			cout << setw(13) << left << mf;
+			cout << setw(13) << left << p;
+			cout << setw(13) << left << ml;
+			cout << "\n";
+			i++;
+
+
+		}
+		else
+		{
+			y = ml ;
+			mr = ml*r/100 ;
+			mf = ml+mr ;
+			ml = mf ;
+			x = ml - mf ;
+			cout << fixed << setprecision(2); 
+			cout << setw(13) << left << i; 
+			cout << setw(13) << left << y;
+			cout << setw(13) << left << mr;
+			cout << setw(13) << left << mf;
+			cout << setw(13) << left << ml;
+			cout << setw(13) << left << x ;
+			cout << "\n";
+			m=0;	
+		}	
+	}
 	
 	return 0;
 }
